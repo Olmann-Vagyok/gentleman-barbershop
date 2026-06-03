cc9d85d7f50e5fdb1b1d65949e81c0d3aaf4a96e
Irakli <irakli@brandr.ge>
2026-06-04 02:08:21 +0400

    Force dynamic on public photos API

--- app/api/photos/route.ts
+++ app/api/photos/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import { getPhotos } from '@/lib/store'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   const photos = await getPhotos()
   return NextResponse.json({ photos })
